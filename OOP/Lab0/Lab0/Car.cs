17a20b2a8f246076065faf285ed7a33115f3f3ad
Ana Calugareanu <ann.calugareanu@gmail.com>
2023-09-11 20:02:06 +0300

    Initial Commit

---
+++ OOP/Lab0/Lab0/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab0
+{
+    public class Car
+    {
+        public decimal Speed { get; set; }
+        public bool IsStateMoving { get => Speed > 0; }
+        public string Color { get; set; }
+
+        public void PrintState()
+        {
+            Console.WriteLine(string.IsNullOrWhiteSpace(Color)
+                              ? "The car has no color"
+                              : "Color is " + Color);
+
+            Console.WriteLine(IsStateMoving
+                              ? $"The car is moving at the speed of {Speed} km/h"
+                              : "The car is not moving");
+        }
+    }
+}
---
+++ OOP/Lab0/Lab0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lab0;
+
+Car car = new Car();
+
+Console.WriteLine("Hi, give me the color that you have in mind for the car: ");
+car.Color = Console.ReadLine();
+
+Console.WriteLine("What is the speed that the car is traveling at:  ");
+car.Speed = Convert.ToDecimal(Console.ReadLine());
+
+car.PrintState();
