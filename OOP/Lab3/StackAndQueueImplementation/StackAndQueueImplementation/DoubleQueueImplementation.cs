6d7e156472b4296bbbdccdb5bf00f6b46cf86b41
Ana Calugareanu <ann.calugareanu@gmail.com>
2023-11-10 13:46:08 +0200

    Implemented DoubleQueueImplementation class

--- OOP/Lab3/StackAndQueueImplementation/StackAndQueueImplementation/DoubleQueueImplementation.cs
+++ OOP/Lab3/StackAndQueueImplementation/StackAndQueueImplementation/DoubleQueueImplementation.cs
@@ -4,33 +4,59 @@ namespace StackAndQueueImplementation
 {
     public class DoubleQueueImplementation : IQueueInterface
     {
-        public bool IsEmpty => throw new NotImplementedException();
+        private List<int> queue;
 
-        public int CountItems => throw new NotImplementedException();
+        public DoubleQueueImplementation()
+        {
+            queue = new List<int>();
+        }
+
+        public bool IsEmpty => queue.Count == 0;
+
+        public int CountItems => queue.Count;
+
+        public int Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The double queue is empty.");
+            }
+
+            return queue[0];
+        }
 
         public int Dequeue()
         {
-            throw new NotImplementedException();
-        }
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The double queue is empty.");
+            }
 
-        public void Enqueue()
-        {
-            throw new NotImplementedException();
+            int frontElement = queue[0];
+            queue.RemoveAt(0);
+            return frontElement;
         }
 
         public void Enqueue(int item)
         {
-            throw new NotImplementedException();
+            queue.Add(item);
         }
 
-        public bool Peek()
+        public int DequeueFromRear()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The double queue is empty.");
+            }
+
+            int rearElement = queue[queue.Count - 1];
+            queue.RemoveAt(queue.Count - 1);
+            return rearElement;
         }
 
-        int IQueueInterface.Peek()
+        public void EnqueueToFront(int item)
         {
-            throw new NotImplementedException();
+            queue.Insert(0, item);
         }
     }
 }
